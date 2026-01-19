5a46c6d06a496bace8ce5e605569cf04733765fd
saurabh007007 <loverboybhol143@gmail.com>
2026-01-19 10:24:04 +0530

    Fix:added two more solution

---
+++ DSA/02_Introduction_cpp/Solution/E.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+ 
+ 
+ 
+int main() {
+   for(int i=1;i<=10;i++){
+    cout<<5*i<<endl;
+   }
+    return 0;
+}
---
+++ DSA/02_Introduction_cpp/Solution/F.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+ 
+ 
+ 
+int main() {
+    int a, b;
+    cin >> a >> b;
+ 
+    cout << "Area = " << a * b << endl;
+    cout << "Perimeter = " << 2 * (a + b) << endl;
+ 
+    return 0;
+}
