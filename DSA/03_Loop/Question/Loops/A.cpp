1f647996e14e27a8f8deb897c6c7d189a49cf75e
saurabh007007 <loverboybhol143@gmail.com>
2026-01-23 03:17:41 +0530

    assignment Loop:A

---
+++ DSA/03_Loop/Question/Loops/A.cpp
@@ -0,0 +1,15 @@
+
+
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+    int t;
+    cin >> t;
+    
+    for (int i = 1; i <=t ; i++) {
+       cout<<i<<endl;
+    }
+    
+    return 0;
+}
