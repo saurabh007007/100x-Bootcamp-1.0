4e93b9c4d6eb519df5f4bf8aa8b4341ce0e8d5ed
saurabh007007 <loverboybhol143@gmail.com>
2026-01-25 12:10:58 +0530

    addeing L solution

---
+++ DSA/03_Loop/Question/Loops/L.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int main(){
+
+    long long n;
+    cin>>n;
+    long long  sum=0;
+    while(n!=0){
+        sum=sum+n%10;
+        n=n/10;
+    }
+    cout<<sum;
+    return 0;
+    }
