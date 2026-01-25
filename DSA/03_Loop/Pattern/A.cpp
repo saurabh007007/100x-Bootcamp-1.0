84c0b0729107e195dbf2e5b5e5a2f253a0c3bc7b
saurabh007007 <loverboybhol143@gmail.com>
2026-01-25 12:42:32 +0530

    Adding loop

---
+++ DSA/03_Loop/Pattern/A.cpp
@@ -0,0 +1,15 @@
+
+#include <bits/stdc++.h>
+ 
+using namespace std;
+ 
+ 
+ 
+int main() {
+    int t;
+    cin >> t;
+   for(int i=0;i<t;i++){
+    cout<<"**"<<endl;
+   }
+    return 0;
+}
---
+++ DSA/03_Loop/Pattern/B.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+ 
+ 
+ 
+int main() {
+    int t,b;
+    cin >> t;
+    cin>>b;
+   for(int i=0;i<t;i++){
+    for(int j=0;j<b;j++){
+        cout<<"*";
+    }
+    cout<<endl;
+   }
+    return 0;
+}
